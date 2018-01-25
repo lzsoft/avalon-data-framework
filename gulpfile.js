0a968ca1333eddd90f2ff9af7de5bb50f569f6e5
JingAn Chen <lzsoft.cja@gmail.com>
2018-01-25 15:26:01 -0500

    Minor.

--- gulpfile.js
+++ gulpfile.js
@@ -8,15 +8,15 @@ gulp.task('js', function () {
             min: '.js'
         },
         noSource: true
-    })).pipe(gulp.dest('./min'));
+    })).pipe(gulp.dest('./min/'));
 });
 gulp.task('css', function () {
     gulp.src('./src/**/*.css').pipe(cleanCSS({
         compatibility: '*'
-    })).pipe(gulp.dest('./min'));
+    })).pipe(gulp.dest('./min/'));
 });
 gulp.task('clean', function () {
-    del('src/', {
+    del('./src/', {
         force: true
     });
 });
